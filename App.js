83be887ae5e2bf0cd68a2a77eb3780907f0703ed
imran hasan <hasanimrantri@gmail.com>
2024-04-08 17:34:20 +0600

    4th-commit

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express")
 const app = express();
 const cors = require("cors")
-const port = 4001;
+const port =process.env.PORT||4001;
 require("./db/connection")
 app.use(cors())
 app.use(express.json())
